bcbbf2f919892b1483d55120ac1fc145c5924264
Tlepbergenova Darya <tdaryat@gmail.com>
2018-10-07 14:43:58 +0700

    class Shape: bugs were fixed

--- Shapes/src/ru/nsu/mmf/g16121/ddt/main/ShapeComparatorArea.java
+++ Shapes/src/ru/nsu/mmf/g16121/ddt/main/ShapeComparatorArea.java
@@ -1,10 +1,12 @@
-package ru.nsu.mmf.g16121.ddt.shape;
+package ru.nsu.mmf.g16121.ddt.main;
+
+import ru.nsu.mmf.g16121.ddt.shape.Shape;
 
 import java.util.Comparator;
 
 public class ShapeComparator implements Comparator<Shape> {
 
     public int compare(Shape shape1, Shape shape2) {
-        return Double.compare(shape1.getArea(),shape2.getArea());
+        return Double.compare(shape1.getArea(), shape2.getArea());
     }
 }
---
+++ Shapes/src/ru/nsu/mmf/g16121/ddt/main/ShapeComparatorPerimeter.java
@@ -0,0 +1,12 @@
+package ru.nsu.mmf.g16121.ddt.main;
+
+import ru.nsu.mmf.g16121.ddt.shape.Shape;
+
+import java.util.Comparator;
+
+public class ShapeComporatorPerimeter implements Comparator<Shape> {
+
+    public int compare(Shape shape1, Shape shape2) {
+        return Double.compare(shape1.getPerimeter(), shape2.getPerimeter());
+    }
+}
