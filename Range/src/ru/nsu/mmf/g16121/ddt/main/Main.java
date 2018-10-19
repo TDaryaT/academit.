627f2938cb29bfb31da03545f8db924971033f53
Tlepbergenova Darya <tdaryat@gmail.com>
2018-10-19 09:36:35 +0700

    class Range are changed

--- Range/src/ru/nsu/mmf/g16121/ddt/main/Main.java
+++ Range/src/ru/nsu/mmf/g16121/ddt/main/Main.java
@@ -7,43 +7,43 @@ import ru.nsu.mmf.g16121.ddt.math.Range;
 public class Main {
     public static void main(String[] args) {
         Range range = new Range(-7, 0);
-
-        System.out.println("Начало диапазона: " + range.getFrom());
-        System.out.println("Конец диапазона: " + range.getTo());
-        System.out.println("Длинна диапазона: " + range.getLength());
-
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Введите число: ");
-        double number = scanner.nextDouble();
-        if (range.isInside(number)) {
-            System.out.println("Число принадлежит диапазону");
-        } else {
-            System.out.println("Число не принадлежит диапазону");
-        }
-        System.out.println();
-
-        System.out.print("Изменим границы диапазона. Введите начало диапазона: ");
-        range.setFrom(scanner.nextDouble());
-        System.out.print("Введите конец диапазона: ");
-        range.setTo(scanner.nextDouble());
-
-        System.out.println("Используем функции для нового диапозона: ");
-
-        System.out.println("Начало диапазона: " + range.getFrom());
-        System.out.println("Конец диапазона: " + range.getTo());
-        System.out.println("Длинна диапазона: " + range.getLength());
-
-        System.out.print("Введите число: ");
-        number = scanner.nextDouble();
-        if (range.isInside(number)) {
-            System.out.println("Число принадлежит диапазону");
-        } else {
-            System.out.println("Число не принадлежит диапазону");
-        }
+//
+//        System.out.println("Начало диапазона: " + range.getFrom());
+//        System.out.println("Конец диапазона: " + range.getTo());
+//        System.out.println("Длинна диапазона: " + range.getLength());
+//
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.print("Введите число: ");
+//        double number = scanner.nextDouble();
+//        if (range.isInside(number)) {
+//            System.out.println("Число принадлежит диапазону");
+//        } else {
+//            System.out.println("Число не принадлежит диапазону");
+//        }
+//        System.out.println();
+//
+//        System.out.print("Изменим границы диапазона. Введите начало диапазона: ");
+//        range.setFrom(scanner.nextDouble());
+//        System.out.print("Введите конец диапазона: ");
+//        range.setTo(scanner.nextDouble());
+//
+//        System.out.println("Используем функции для нового диапозона: ");
+//
+//        System.out.println("Начало диапазона: " + range.getFrom());
+//        System.out.println("Конец диапазона: " + range.getTo());
+//        System.out.println("Длинна диапазона: " + range.getLength());
+//
+//        System.out.print("Введите число: ");
+//        number = scanner.nextDouble();
+//        if (range.isInside(number)) {
+//            System.out.println("Число принадлежит диапазону");
+//        } else {
+//            System.out.println("Число не принадлежит диапазону");
+//        }
 
         Range range1 = new Range(1, 6);
-        Range range2 = new Range(5, 8);
+        Range range2 = new Range(6, 8);
 
         System.out.print("Пересечение диапозонов: ");
         range1.print();
@@ -53,6 +53,8 @@ public class Main {
         range = Range.getIntersection(range1, range2);
         if (range != null) {
             range.print();
+        } else{
+            System.out.println("его нет");
         }
         System.out.println();
 
--- Range/src/ru/nsu/mmf/g16121/ddt/math/Range.java
+++ Range/src/ru/nsu/mmf/g16121/ddt/math/Range.java
@@ -34,28 +34,28 @@ public class Range {
     }
 
     public static Range getIntersection(Range range1, Range range2) {
-        if ((range1.to < range2.from) || (range2.to < range1.from)) {
+        if ((range1.to <= range2.from) || (range2.to <= range1.from)) {
             return null;
         } else {
-            double to = Math.max(range1.to, range2.to);
-            double from = Math.min(range1.from, range2.from);
-            return new Range(to, from);
+            double from = Math.max(range1.from, range2.from);
+            double to = Math.min(range1.to, range2.to);
+            return new Range(from, to);
         }
     }
 
     public static Range[] getUnion(Range range1, Range range2) {
         if ((range1.to < range2.from) || (range2.to < range1.from)) {
-            return new Range[]{new Range(range1.to, range1.from), new Range(range2.to, range2.from)};
+            return new Range[]{new Range(range1.from, range1.to), new Range(range2.from, range2.to)};
         } else {
-            double to = Math.min(range1.to, range2.to);
-            double from = Math.max(range1.from, range2.from);
-            return new Range[]{new Range(to, from)};
+            double from = Math.min(range1.from, range2.from);
+            double to = Math.max(range1.to, range2.to);
+            return new Range[]{new Range(from, to)};
         }
     }
 
     public static Range[] getDifference(Range range1, Range range2) {
         if ((range1.to < range2.from) || (range2.to < range1.from)) {
-            return new Range[]{new Range(range1.to, range1.from)};
+            return new Range[]{new Range(range1.from, range1.to)};
         } else if (range1.from >= range2.from && range1.to <= range2.to) {
             return new Range[0];
         } else if (range2.from > range1.from && range2.to < range1.to) {
@@ -63,7 +63,7 @@ public class Range {
         } else if (range2.from > range1.from) {
             return new Range[]{new Range(range1.from, range2.from)};
         } else {
-            return new Range[]{new Range(range2.to, range1.from)};
+            return new Range[]{new Range(range2.to, range1.to)};
         }
     }
 
