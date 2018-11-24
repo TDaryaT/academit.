e8eed051bfbb9b0f3436426b2fb0e6da7a255cde
Darya <tdaryat@gmail.com>
2018-11-24 15:40:01 +0600

    class Range are changed
    func getDifference

--- Range/src/ru/nsu/mmf/g16121/ddt/math/Range.java
+++ Range/src/ru/nsu/mmf/g16121/ddt/math/Range.java
@@ -54,10 +54,9 @@ public class Range {
     }
 
     public static Range[] getDifference(Range range1, Range range2) {
-        if ((range1.to < range2.from) || (range2.to < range1.from)) {
+        if ((range1.to <= range2.from) || (range2.to <= range1.from)) {
             return new Range[]{new Range(range1.from, range1.to)};
-        } else if ((range1.from >= range2.from && range1.to <= range2.to) ||
-                (range1.to == range2.from || range2.to == range1.from)) {
+        } else if (range1.from >= range2.from && range1.to <= range2.to){
             return new Range[0];
         } else if (range2.from > range1.from && range2.to < range1.to) {
             return new Range[]{new Range(range1.from, range2.from), new Range(range2.to, range1.to)};
