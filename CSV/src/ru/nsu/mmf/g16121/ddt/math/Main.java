96c586ac918234071dec3d15b6892d07321b47d4
TDaryaT <tdaryat@gmail.com>
2020-08-08 01:53:54 +0300

    CSV fixed, all works

--- CSV/src/ru/nsu/mmf/g16121/ddt/math/Main.java
+++ CSV/src/ru/nsu/mmf/g16121/ddt/math/Main.java
@@ -5,6 +5,6 @@ import static ru.nsu.mmf.g16121.ddt.scv.CSV.convertToHTML;
 
 public class Main {
     public static void main(String[] args)throws FileNotFoundException {
-        convertToHTML("//home//dasha//IdeaProjects//academit.//CSV//inHtml.html","//home//dasha//IdeaProjects//academit.//CSV//inCSV_bcp.csv");
+        convertToHTML("//home//dasha//IdeaProjects//academit.//CSV//test.html","//home//dasha//IdeaProjects//academit.//CSV//test.csv");
     }
 }
